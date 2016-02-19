df9a9063760747cad63a50d943a9d7f2093f1f3e
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-02-19 09:27:14 -0800

    Handle when no assignment is present

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -3,4 +3,8 @@ class Inspection < ActiveRecord::Base
   has_many :assignments
   has_many :inspectors, through: :assignments
   accepts_nested_attributes_for :address
+
+  def current_assignment
+    self.assignments.first || nil
+  end
 end
