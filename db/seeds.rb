d7ce38327bd5d113c5b4e1966b6dc8d114b85538
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-02-19 08:30:38 -0800

    Beginning to separate requester and inspector experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Inspectors
+User.create(name: "Dale Wiersma", email: "dale@longbeach.gov", role: "inspector")
+User.create(name: "Steven Valdez", email: "steven@longbeach.gov", role: "inspector")
+
+# Test users
+User.create(name: "Mark Rossetti", email: "markrossetti@codeforamerica.org", role: "requester")
+User.create(name: "Patrick McDonnell", email: "patrick@codeforamerica.org", role: "requester")
+User.create(name: "Lisa Ratner", email: "lisa@codeforamerica.org", role: "requester")
+
