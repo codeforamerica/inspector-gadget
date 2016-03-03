1f41441692dbe6399a264474d01f11373dfc207c
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-03-03 11:22:34 -0800

    Inspectors now know about their inspections (geo filter only)

--- app/models/inspector.rb
+++ app/models/inspector.rb
@@ -2,4 +2,12 @@ class Inspector < User
   has_one :inspector_profile
 
   delegate :inspection_categories, to: :inspector_profile
+
+  def inspections
+    if (profile = self.inspector_profile).present?
+      Inspection.joins(:address).where("ST_Covers(?, addresses.geo_location)", profile.inspection_region.to_s)
+    else
+      []
+    end
+  end
 end
