f3af5a1adf58e69d8c302cc4cabb2db8c3cfa8a2
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-13 12:55:24 -0700

    Removed query to find inspections from Inspector - workaround for now

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -20,6 +20,9 @@ class InspectionsController < ApplicationController
 
   def report
     @inspections = Inspection.where("date_trunc('day', requested_for_date) = ?", params[:report_date])
+    @inspection_assignments = @inspections.group_by do |inspection|
+      inspection.inspector
+    end
   end
 
   # GET /inspections/1
