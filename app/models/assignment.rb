c3e6576b8a805c427b26cec0d07e11ba843280e6
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-14 15:02:56 -0700

    Remove unused Assignment model

--- app/models/assignment.rb
+++
@@ -1,4 +0,0 @@
-class Assignment < ActiveRecord::Base
-  belongs_to :inspection
-  belongs_to :inspector, class_name: "User"
-end
--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,8 +1,6 @@
 class Inspection < ActiveRecord::Base
   has_one :address
   belongs_to :inspection_type
-  has_many :assignments
-  has_many :inspectors, through: :assignments
   accepts_nested_attributes_for :address
 
   validates :inspection_type_id, presence: true
