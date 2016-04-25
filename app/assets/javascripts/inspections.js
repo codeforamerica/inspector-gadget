9a387c1e8b59c893d3d1edfc578ef8c685c26e10
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-25 09:51:25 -0700

    Style and usability improvements

--- app/assets/javascripts/inspections.js
+++ app/assets/javascripts/inspections.js
@@ -14,7 +14,7 @@ function enableInspectionDropdowns () {
       var uniq_types = _.uniqBy(inspection_types, function (i) { return i.inspection_category; })
 
       // set up subsequent dropdown (categories) with options
-      $('.inspection-categories').show().html( $("<option>") ) // start with a blank
+      $('.inspection-categories').show().html( $("<option value='', disabled='true', selected='true'>Select an inspection category</option>") ) // start with a blank
       $('.inspection-names').html( $("<option>") ) // start with a blank
       $(uniq_types).each(function (index, type) {
         $('.inspection-categories').append( $("<option>", { value: type.inspection_category, html: type.inspection_category_name }) )
@@ -27,7 +27,7 @@ function enableInspectionDropdowns () {
     $.getJSON('/api/v1/inspection_types?supercategory='+supercategory+'&category='+this.value, function (inspection_types) {
 
       // set up subsequent dropdown (names) with options
-      $('.inspection-names').show().html( $("<option>") ) // start with a blank
+      $('.inspection-names').show().html( $("<option value='', disabled='true', selected='true'>Select an inspection type</option>") ) // start with a blank
       $(inspection_types).each(function (index, type) {
         $('.inspection-names').append( $("<option>", { value: type.id, html: type.inspection_name, "data-comments": type.comments }) )
       })
