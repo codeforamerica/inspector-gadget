660a3e65d5d6fd976854ba197b995dd5284fd311
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-05-25 10:55:14 -0700

    Stricter time zone use to get same behavior across local and remote servers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,9 +6,9 @@ module ApplicationHelper
   def last_inspection_day_date
     if !Date.today.weekday?
       return 0.business_day.ago.to_date
-    elsif Time.zone.now < BusinessTime::Config.beginning_of_workday.to_time
+    elsif Time.zone.now < BusinessTime::Config.beginning_of_workday.in_time_zone.to_time
       return 0.business_day.ago.to_date
-    elsif Time.zone.now > BusinessTime::Config.end_of_workday.to_time
+    elsif Time.zone.now > BusinessTime::Config.end_of_workday.in_time_zone.to_time
       return 1.business_day.ago.to_date # I have no idea why this is right, but it has tests :-)
     else # during business hours
       return 1.business_day.ago.to_date
@@ -16,9 +16,9 @@ module ApplicationHelper
   end
 
   def next_inspection_day_date
-    if Time.zone.now < BusinessTime::Config.beginning_of_workday.to_time 
+    if Time.zone.now < BusinessTime::Config.beginning_of_workday.in_time_zone.to_time
       return 1.business_day.from_now.to_date
-    elsif Time.zone.now > BusinessTime::Config.end_of_workday.to_time
+    elsif Time.zone.now > BusinessTime::Config.end_of_workday.in_time_zone.to_time
       return 0.business_day.from_now.to_date
     else # during business hours
       return 1.business_day.from_now.to_date
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,37 +3,37 @@ require 'rails_helper'
 describe ApplicationHelper do
   describe '#last_inspection_day_date' do
     it 'should return Tuesday on a Wednesday before working hours' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 2) ) do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 2) ) do
         expect( last_inspection_day_date ).to eq( tuesday );
       end
     end
 
     it 'should return Tuesday on a Wednesday before 3pm' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 14, minutes: 30) ) do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 14, minutes: 30) ) do
         expect( last_inspection_day_date ).to eq( tuesday );
       end
     end
 
     it 'should return Tuesday on a Wednesday after 3pm' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 18) ) do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 18) ) do
         expect( last_inspection_day_date ).to eq( tuesday );
       end
     end
 
     it 'should return Friday on a Monday before 3pm' do
-      Timecop.freeze( monday.to_time.in_time_zone.advance(hours: 14, minutes: 30) ) do
+      Timecop.freeze( monday.in_time_zone.advance(hours: 14, minutes: 30) ) do
         expect( last_inspection_day_date ).to eq( friday );
       end
     end
 
     it 'should return Friday on a Monday after 3pm' do
-      Timecop.freeze( monday.to_time.in_time_zone.advance(hours: 18) ) do
+      Timecop.freeze( monday.in_time_zone.advance(hours: 18) ) do
         expect( last_inspection_day_date ).to eq( friday );
       end
     end
 
     it 'should return Friday on a Saturday after 3pm' do
-      Timecop.freeze( Date.civil(2016, 5, 21).to_time.in_time_zone.advance(hours: 18) ) do
+      Timecop.freeze( saturday.in_time_zone.advance(hours: 18) ) do
         expect( last_inspection_day_date ).to eq( friday );
       end
     end
@@ -41,37 +41,37 @@ describe ApplicationHelper do
 
   describe '#next_inspection_day_date' do
     it 'should return Thursday on a Wednesday before working hours' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 2) ) do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 2) ) do
         expect( next_inspection_day_date ).to eq( Date.civil(2016, 5, 26) );
       end
     end
 
-    it 'should return Thursday on a Wednesday before 3pm' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 14, minutes: 30) ) do
+    it 'should return Thursday on a Wednesday during working hours' do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 14, minutes: 30) ) do
         expect( next_inspection_day_date ).to eq( Date.civil(2016, 5, 26) );
       end
     end
 
-    it 'should return Thursday on a Wednesday after 3pm' do
-      Timecop.freeze( wednesday.to_time.in_time_zone.advance(hours: 18) ) do
+    it 'should return Thursday on a Wednesday after working hours' do
+      Timecop.freeze( wednesday.in_time_zone.advance(hours: 18) ) do
         expect( next_inspection_day_date ).to eq( Date.civil(2016, 5, 26) );
       end
     end
 
     it 'should return Monday on a Friday before 3pm' do
-      Timecop.freeze( friday.to_time.in_time_zone.advance(hours: 14, minutes: 30) ) do
+      Timecop.freeze( friday.in_time_zone.advance(hours: 14, minutes: 30) ) do
         expect( next_inspection_day_date ).to eq( monday );
       end
     end
 
     it 'should return Monday on a Friday after 3pm' do
-      Timecop.freeze( friday.to_time.in_time_zone.advance(hours: 18) ) do
+      Timecop.freeze( friday.in_time_zone.advance(hours: 18) ) do
         expect( next_inspection_day_date ).to eq( monday );
       end
     end
 
     it 'should return Monday on a Sunday after 3pm' do
-      Timecop.freeze( friday.to_time.in_time_zone.advance(hours: 18) ) do
+      Timecop.freeze( friday.in_time_zone.advance(hours: 18) ) do
         expect( next_inspection_day_date ).to eq( monday );
       end
     end
@@ -82,6 +82,10 @@ end
 
 def friday
   Date.civil(2016, 5, 20)
+end
+
+def saturday
+  Date.civil(2016, 5, 21)
 end
 
 def monday
