0359e0afcc59157cc49f11cc54b5acdc2b0c691e
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-06-16 00:16:41 -0700

    Temp patch for iframe CSRF error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  # TODO: Reactivate this
+  # Causing severe problems when the form is iframed
+  # And at the moment, it's not the weakest security link anyway
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 end
