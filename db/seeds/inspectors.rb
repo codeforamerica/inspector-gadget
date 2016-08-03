ff3686c342d1f443bb312501beccc292a35adddb
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-08-03 14:34:44 -0700

    Fixed Inspector seeds to match new users table from devise

--- db/seeds/inspectors.rb
+++ db/seeds/inspectors.rb
@@ -1,51 +1,115 @@
 ##### INSPECTORS
 
-inspector = Inspector.find_or_create_by(name: "Engler")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Engler') ||
+            Inspector.create(name: 'Engler', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Lloyd")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Lloyd') ||
+            Inspector.create(name: 'Lloyd', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Morey")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Morey') ||
+            Inspector.create(name: 'Morey', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Ciarrelli")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Ciarrelli') ||
+            Inspector.create(name: 'Ciarrelli', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Reza")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Reza') ||
+            Inspector.create(name: 'Reza', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Flacks")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Flacks') ||
+            Inspector.create(name: 'Flacks', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Mann")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['residential_general'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Mann') ||
+            Inspector.create(name: 'Mann', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['residential_general'])
 
-inspector = Inspector.find_or_create_by(name: "Aaker")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential').update_attributes(inspection_assignments: ['photovoltaic'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Aaker') ||
+            Inspector.create(name: 'Aaker', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'residential')
+                .update_attributes(inspection_assignments: ['photovoltaic'])
 
 # ----------
 
-inspector = Inspector.find_or_create_by(name: "Ingram")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['electric', 'sign']) #'photovoltaic' for all areas
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Ingram') ||
+            Inspector.create(name: 'Ingram', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: %w(electric sign)) # 'photovoltaic' for all areas
 
-inspector = Inspector.find_or_create_by(name: "Varnes")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['electric'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Varnes') ||
+            Inspector.create(name: 'Varnes', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['electric'])
 
-inspector = Inspector.find_or_create_by(name: "Mechanical")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['mechanical'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Mechanical') ||
+            Inspector.create(name: 'Mechanical', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['mechanical'])
 
-inspector = Inspector.find_or_create_by(name: "Marquez")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['building']) # roof, foundation, drywall
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Marquez') ||
+            Inspector.create(name: 'Marquez', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['building']) # roof, foundation, drywall
 
-inspector = Inspector.find_or_create_by(name: "Nicholls")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['building']) # roof, foundation, drywall
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Nicholls') ||
+            Inspector.create(name: 'Nicholls', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['building']) # roof, foundation, drywall
 
-inspector = Inspector.find_or_create_by(name: "Plumbing")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['plumbing'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Plumbing') ||
+            Inspector.create(name: 'Plumbing', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['plumbing'])
 
-inspector = Inspector.find_or_create_by(name: "Joe")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['fire'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'Joe') ||
+            Inspector.create(name: 'Joe', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['fire'])
 
-inspector = Inspector.find_or_create_by(name: "PLANNING")
-InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial').update_attributes(inspection_assignments: ['planning'])
+new_email = Devise.friendly_token.first(6)
+new_password = Devise.friendly_token.first(12)
+inspector = Inspector.find_by(name: 'PLANNING') ||
+            Inspector.create(name: 'PLANNING', email: "#{new_email}@example.com", password: new_password, password_confirmation: new_password)
+InspectorProfile.find_or_create_by(inspector_id: inspector.id, inspector_type: 'commercial')
+                .update_attributes(inspection_assignments: ['planning'])
