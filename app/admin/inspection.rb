33f5801cee0b6c3d6102578404f44fd7697bf70e
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-08-03 10:59:43 -0700

    Ability to edit inspections w/address in activeadmin

--- app/admin/inspection.rb
+++ app/admin/inspection.rb
@@ -1,5 +1,20 @@
 ActiveAdmin.register Inspection do
 
+  permit_params :permit_number,
+                :contact_name,
+                :contact_phone,
+                :contact_email,
+                :requested_for_date,
+                address_attributes: [
+                  :street_number,
+                  :route,
+                  :city,
+                  :state,
+                  :zip,
+                ]
+
+  filter :requested_for_date
+
   index do
     selectable_column
     id_column
@@ -10,6 +25,45 @@ ActiveAdmin.register Inspection do
     actions
   end
 
-  filter :requested_for_date
+  show do
+    attributes_table_for inspection do
+      row :permit_number
+      row :contact_name
+      row :contact_phone
+      row :contact_email
+      row :requested_for_date
+      row :requested_for_time
+      row :created_at
+      row :contact_phone_can_text
+      row :inspection_notes
+    end
+    attributes_table_for inspection.address do
+      row :city
+      row :zip
+    end
+  end
 
+  form do |f|
+    f.inputs 'Inspection' do
+      f.input :permit_number
+      f.input :contact_name
+      f.input :contact_phone
+      f.input :contact_email
+      f.input :requested_for_date
+      f.input :requested_for_time
+      f.input :contact_phone_can_text
+      f.input :inspection_notes
+    end
+
+    f.inputs 'Address' do
+      f.has_many :address do |a|
+        a.input :street_number
+        a.input :route
+        a.input :city
+        a.input :state
+        a.input :zip
+      end
+    end
+    f.actions
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20160802232427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "postgis"
   enable_extension "pg_stat_statements"
+  enable_extension "postgis"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
