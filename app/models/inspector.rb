890eb1459b8b90de84be0f64c09252ee96cad4bb
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-10-28 13:49:00 -0700

    Note about STI on Inspector model

--- app/models/inspector.rb
+++ app/models/inspector.rb
@@ -1,5 +1,9 @@
+# This model shares the `users` table via single-table inheritance (STI)
+# This makes it easy for Inspectors to log in as Users in future, if desired
 class Inspector < User
-  has_one :inspector_profile
+  # fields specific to an Inspector (i.e. not general to all Users) lives in the `inspector_profile`
+  has_one :inspector_profile 
+  
   delegate :inspection_types, to: :inspector_profile
   delegate :inspection_assignments, to: :inspector_profile
 
